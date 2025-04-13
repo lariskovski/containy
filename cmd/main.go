d3a2022d6c13a7380520a06d339e85fef8c3fa21
Larissa <37162993+lariskovski@users.noreply.github.com>
2025-04-13 23:39:16 +0000

    style: go fmt

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,6 @@ import (
 
 var rootfsUrl = "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-minirootfs-3.21.3-x86_64.tar.gz"
 
-
 func main() {
 	if os.Args[0] == "/proc/self/exe" {
 		fmt.Println("In child process:")
@@ -72,31 +71,31 @@ func must(err error) {
 
 func fsSetup() {
 	err := utils.CreateDirectory("lower", "upper", "work", "merged")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error creating directories: %v\n", err)
-        os.Exit(1)
-    }
-    fmt.Println("Directories created successfully.")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating directories: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Directories created successfully.")
 
-    // Download the root filesystem
-    err = utils.DownloadRootFS(rootfsUrl, "lower")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error downloading root filesystem: %v\n", err)
-        os.Exit(1)
-    }
-    fmt.Println("Root filesystem downloaded successfully.")
+	// Download the root filesystem
+	err = utils.DownloadRootFS(rootfsUrl, "lower")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error downloading root filesystem: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Root filesystem downloaded successfully.")
 
-    // Create the overlay filesystem
-    overlayFS := &overlay.OverlayFS{
-        LowerDir:  "lower",
-        UpperDir:  "upper",
-        WorkDir:   "work",
-        MergedDir: "merged",
-    }
-    err = overlayFS.Mount()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error creating overlay filesystem: %v\n", err)
-        os.Exit(1)
-    }
-    fmt.Println("Overlay filesystem created successfully.")
+	// Create the overlay filesystem
+	overlayFS := &overlay.OverlayFS{
+		LowerDir:  "lower",
+		UpperDir:  "upper",
+		WorkDir:   "work",
+		MergedDir: "merged",
+	}
+	err = overlayFS.Mount()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating overlay filesystem: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Overlay filesystem created successfully.")
 }
