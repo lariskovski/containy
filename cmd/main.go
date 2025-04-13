e06b30c668c4f46064de019d10bacbfc759eab3b
Larissa <37162993+lariskovski@users.noreply.github.com>
2025-04-13 20:32:43 +0000

    feat: implement child process handling and namespace isolation in main function

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,72 @@ package main
 import (
 	"fmt"
 	"os"
-
+	"os/exec"
+	"syscall"
 	"github.com/lariskovski/containy/internal/overlay"
 	"github.com/lariskovski/containy/internal/utils"
 )
 
 var rootfsUrl = "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-minirootfs-3.21.3-x86_64.tar.gz"
 
+
 func main() {
-    err := utils.CreateDirectory("lower", "upper", "work", "merged")
+	if os.Args[0] == "/proc/self/exe" {
+		fmt.Println("In child process:")
+		must(syscall.Sethostname([]byte("container")))
+
+		// Mount necessary filesystems
+		must(syscall.Mount("proc", "/proc", "proc", 0, ""))
+		must(syscall.Mount("tmpfs", "/dev", "tmpfs", 0, ""))
+
+		// Perform pivot_root
+		must(os.MkdirAll("merged/oldroot", 0755))
+		must(syscall.PivotRoot("merged", "merged/oldroot"))
+		must(os.Chdir("/")) // Change working directory to new root
+
+		// Unmount old root
+		must(syscall.Unmount("oldroot", syscall.MNT_DETACH))
+		must(os.Remove("oldroot"))
+
+		// Set PATH environment variable
+		must(os.Setenv("PATH", "/bin:"+os.Getenv("PATH")))
+
+		// Start a shell
+		cmd := exec.Command("sh")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		must(cmd.Run())
+		return
+	}
+
+	fsSetup()
+
+	fmt.Println("Spawning child with new namespaces...")
+
+	cmd := exec.Command("/proc/self/exe") // self-exec trick
+	cmd.Args = []string{"/proc/self/exe"}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWPID,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func fsSetup() {
+	err := utils.CreateDirectory("lower", "upper", "work", "merged")
     if err != nil {
         fmt.Fprintf(os.Stderr, "Error creating directories: %v\n", err)
         os.Exit(1)
@@ -39,5 +96,4 @@ func main() {
         os.Exit(1)
     }
     fmt.Println("Overlay filesystem created successfully.")
-
 }
