fd9412a27f9a76e1b4a2d75537759e7f7d309660
Larissa <37162993+lariskovski@users.noreply.github.com>
2025-04-14 18:07:22 +0000

    fix: update .gitignore and remove obsolete cleanup.sh script

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-sudo umount /workspaces/containy/tmp/build/layers/6dd911f334/merged 
-sudo umount /workspaces/containy/tmp/build/layers/b0bf345897/merged 
-sudo umount /workspaces/containy/tmp/build/layers/944c032886/merged
-# sudo rm -rf merged lower upper work
-sudo rm -rf tmp
