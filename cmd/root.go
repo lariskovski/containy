78c0d4a2df1d41e9536e197be75d0765572690fc
Larissa <37162993+lariskovski@users.noreply.github.com>
2025-04-25 21:24:22 -0300

    refactor: improve cli commands organization (#4)

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,24 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-func Execute() {
-	var rootCmd = &cobra.Command{Use: "containy"}
-	rootCmd.AddCommand(
-		NewBuildCmd(),
-		NewRunCmd(),
-	)
+var rootCmd = &cobra.Command{
+	Use: "containy",
+	Short: "Containy is a container runtime",
+	Long:  "Containy is a container runtime that allows you to build and run containers.",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
 
+func Execute() {
 	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
